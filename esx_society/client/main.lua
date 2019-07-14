7fe0f9df93a04c2dc3d8012515a6d5927eb8864e
EmpireLuigi <46849323+EmpireLuigi@users.noreply.github.com>
2019-07-14 16:25:49 -0500

    Update main.lua

--- esx_society/client/main.lua
+++ esx_society/client/main.lua
@@ -451,17 +451,3 @@ AddEventHandler('esx_society:openBossMenu', function(society, close, options)
 	OpenBossMenu(society, close, options)
 end)
 
---[[Citizen.CreateThread(function()
-    while true do
-    Citizen.Wait(0)
-        if IsPedInAnyPoliceVehicle(GetPlayerPed(PlayerId())) then
-            local veh = GetVehiclePedIsUsing(GetPlayerPed(PlayerId()), false)
-            if (GetPedInVehicleSeat(veh, -1) == GetPlayerPed(PlayerId())) then
-                if (PlayerData.job ~= nil and (PlayerData.job.name ~= 'police' or PlayerData.job.name ~= 'ambulance')) then
-                  ESX.ShowNotification("Only Cops can drive this yo..")
-                  SetVehicleUndriveable(veh, true)
-                end
-            end
-        end
-    end
-end)]]--
